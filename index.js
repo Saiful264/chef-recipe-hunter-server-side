a560ab5ee0e9560ee2294e1c00156bc952a6e1d8
Saiful264 <saifulislamx264@gmail.com>
2023-05-03 00:48:42 +0600

    pass the single chef data

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+// const cors = require('cors');
 const port = 5000
+
+
+const  chefs = require("./data/data.json");
+
+// app.use(cors());
 
 app.get("/", (req, res)=>{
     res.send("Server is runing");
 })
 
-app.get("/")
+app.get("/chefs", (req, res)=>{
+    res.send(chefs);
+})
 
+app.get('/chefs/:id', (req, res)=>{
+    const id = req.params.id;
+
+    const singleChef = chefs.find((c)=> c._id === id);
+    
+    res.send(singleChef);
+})
 
 app.listen(port, ()=>{
     console.log(`Server is runing on port ${port}`);
