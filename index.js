7b5685d9d66e19b9ad8a1589a1879d94b9211b7f
Saiful264 <saifulislamx264@gmail.com>
2023-05-04 12:22:02 +0600

    install cors and useing cors in server

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors');
+const cors = require('cors'); //before useing the cors make sure install cors commend => npm install cars
 const port = process.env.PORT || 5000
 
 
 const  chefs = require("./data/data.json");
 
-// app.use(cors());
+app.use(cors());
 
 app.get("/", (req, res)=>{
     res.send("Server is runing");
 })
 
-app.get("/chefs", (req, res)=>{
+app.get("/chef's", (req, res)=>{
     res.send(chefs);
 })
 
-app.get('/chefs/:id', (req, res)=>{
+app.get("/chef's/:id", (req, res)=>{
     const id = req.params.id;
 
     const singleChef = chefs.find((c)=> c._id === id);
